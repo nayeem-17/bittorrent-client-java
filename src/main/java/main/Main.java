10922c5a83b47dcab28dd149f7b4417b5e111891
Md. Mehrab Haque <mdmehrabhaque@gmail.com>
2021-03-04 14:09:00 +0600

    added torrent accessor

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,6 +1,7 @@
 package main;
 
 import torrent.Downloader;
+import torrent.libs.TorrentFile;
 
 import java.io.IOException;
 
@@ -9,6 +10,7 @@ public class Main {
         Downloader downloader=new Downloader("https://yts.mx//torrent//download//6BBBA12D4052E322E4A04F303581193DB957EAED");
         try {
             downloader.download();
+            TorrentFile torrentFile= downloader.getTorrentFile();
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/main/java/torrent/Downloader.java
+++ src/main/java/torrent/Downloader.java
@@ -9,6 +9,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -16,11 +17,12 @@ public class Downloader {
 
     private final String FILE_NAME="tmp.torrent";
     private String url;
+    private TorrentFile torrentFile;
+
+
     public Downloader(String url){
         this.url=url;
-
     }
-
     public void download() throws IOException {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
              FileOutputStream fileOutputStream = new FileOutputStream(FILE_NAME)) {
@@ -30,23 +32,13 @@ public class Downloader {
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
             }
         } catch (IOException e) {
-            // handle exception
             e.printStackTrace();
         }
-
         File rawFile = new File(FILE_NAME);
-        TorrentFile torrentFile=new TorrentFile(rawFile);
-        System.out.println(torrentFile.getNumPieces());
-
-
-
-        /*String input = torrentString;
-        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
-        BencodeInputStream bencode = new BencodeInputStream(in);
-
-        Type type = bencode.nextType(); // Returns Type.DICTIONARY
-        Map<String, Object> dict = bencode.readDictionary();
-
-        System.out.println(dict);*/
+        torrentFile=new TorrentFile(rawFile);
+        rawFile.delete();
+    }
+    public TorrentFile getTorrentFile(){
+        return torrentFile;
     }
 }
