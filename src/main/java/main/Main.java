7311ad6d1e6e1471e6ee76bf1791bcbcb3d17ee8
nayeem-17 <nasa.28apr@gmail.com>
2021-03-04 14:32:45 +0600

    aded gitignore

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -6,11 +6,12 @@ import torrent.libs.TorrentFile;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args){
-        Downloader downloader=new Downloader("https://yts.mx//torrent//download//6BBBA12D4052E322E4A04F303581193DB957EAED");
+    public static void main(String[] args) {
+        Downloader downloader = new Downloader(
+                "https://yts.mx//torrent//download//6BBBA12D4052E322E4A04F303581193DB957EAED");
         try {
             downloader.download();
-            TorrentFile torrentFile= downloader.getTorrentFile();
+            TorrentFile torrentFile = downloader.getTorrentFile();
         } catch (IOException e) {
             e.printStackTrace();
         }
